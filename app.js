33d6337683fd6dd3a6d51bf28b9c0f2302d9ada9
SyedBashar <MhuammadBashar@gmail.com>
2024-10-17 16:59:22 +0500

    Task Done Range

---
+++ app.js
@@ -0,0 +1,11 @@
+var number = prompt("Enter your Range")
+if (number <= 10) {
+    alert("The number is between 1 and 10")
+
+}
+else if (number > 10 && number <= 20) {
+    alert("The number is between 11 and 20")
+}
+else {
+    alert("The number is out of range")
+}
